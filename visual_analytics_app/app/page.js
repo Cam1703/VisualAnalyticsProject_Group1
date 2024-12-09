07aff7df728c081d7adefb65d8efca2d42ad7946
Cam1703 <a20180103@pucp.edu.pe>
2024-12-09 15:49:09 +0100

    React Project Creation

---
+++ visual_analytics_app/app/page.js
@@ -0,0 +1,12 @@
+
+/*
+  Visual Analytics Dashboard
+*/
+
+export default function Home() {
+  return (
+    <main className=" p-2 flex justify-center ">
+      <h1 className=" text-4xl font-bold ">Visual Analytics</h1>
+    </main>
+  );
+}
