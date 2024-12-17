24644fd5bd6c4ddc183acbf32583a6d962667b02
julia-stancioli-sapienza <stanciolipaiva.2175608@studenti.uniroma1.it>
2024-12-17 17:45:00 +0100

    Reading all original files (since 1991) - male players

---
+++ visual_analytics_app/data_processing/process_original_files.py
@@ -0,0 +1,29 @@
+# %% Importing packages
+import pandas as pd
+import numpy as np
+import os
+
+pd.options.display.max_columns = None
+
+# %%
+# Method to read original files
+def read_all_files(directory):
+
+    # List of all files in the directory
+    csv_files = [file for file in os.listdir(directory)]
+
+    dataframes = []
+
+    for file in csv_files:
+
+        # Full file path for each file
+        complete_path = os.path.join(directory, file)
+        dataframes.append(pd.read_csv(complete_path, encoding='ISO-8859-1'))
+
+    # Joins separate files in single dataframe
+    complete_dataset = pd.concat(dataframes)
+
+    return complete_dataset
+
+# %%
+complete_dataset = read_all_files('../data')
