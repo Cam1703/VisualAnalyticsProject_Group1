d2deace6d1ebbfd8af08227e8eae0b76a93b1e4f
julia-stancioli-sapienza <stanciolipaiva.2175608@studenti.uniroma1.it>
2024-12-18 00:57:55 +0100

    Column selection

--- visual_analytics_app/data_processing/process_original_files.py
+++ visual_analytics_app/data_processing/process_original_files.py
@@ -27,3 +27,41 @@ def read_all_files(directory):
 
 # %%
 complete_dataset = read_all_files('../data')
+
+# %% Filtering only interesting columns, separating winners and losers
+basic_columns = [
+    'tourney_name',
+    'surface',
+    'tourney_level',
+    'tourney_date',
+    'score',
+    'round'
+]
+player_column_suffixes = [
+    'id',
+    'name',
+    'hand',
+    'ht',
+    'ioc',
+    'rank',
+    'rank_points'
+]
+serve_column_suffixes = [
+    'ace',
+    'df',
+    'svpt',
+    '1stIn',
+    '1stWon',
+    '2ndWon',
+    'SvGms',
+    'bpSaved',
+    'bpFaced'
+]
+
+winners_profile_columns = ['winner_' + suffix for suffix in player_column_suffixes]
+winners_serve_columns = ['w_' + suffix for suffix in serve_column_suffixes]
+losers_profile_columns = ['loser_' + suffix for suffix in player_column_suffixes]
+losers_serve_columns = ['l_' + suffix for suffix in serve_column_suffixes]
+
+winners_data = complete_dataset[basic_columns + winners_profile_columns + winners_serve_columns]
+losers_data = complete_dataset[basic_columns + losers_profile_columns + losers_serve_columns]
