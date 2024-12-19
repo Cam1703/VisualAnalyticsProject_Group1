ff75fef1e92bb78ae62eb83dffcca89ac16dab82
julia-stancioli-sapienza <stanciolipaiva.2175608@studenti.uniroma1.it>
2024-12-19 13:50:21 +0100

    Removing prefix of columns (winner, loser)

--- visual_analytics_app/data_processing/process_original_files.py
+++ visual_analytics_app/data_processing/process_original_files.py
@@ -25,8 +25,22 @@ def read_all_files(directory):
 
     return complete_dataset
 
+
+# Obtain the year of the tournament
+def extract_year(date_value):
+    return str(int(date_value))[:4]
+
+# Removing the prefix of the columns
+def remove_prefix(data, prefix):
+    size = len(prefix)
+    data.columns = [col[size:] if col.startswith(prefix) else col for col in data.columns]
+
 # %%
 complete_dataset = read_all_files('../data')
+complete_dataset['tourney_year'] = complete_dataset['tourney_date'].apply(extract_year)
+
+# Filtering only Grand-Slams, Masters 1000, ATP 500 and 250
+filtered_tournaments = complete_dataset[complete_dataset['tourney_level'].isin(['G', 'M', 'A'])].copy()
 
 # %% Filtering only interesting columns, separating winners and losers
 basic_columns = [
@@ -35,7 +49,8 @@ basic_columns = [
     'tourney_level',
     'tourney_date',
     'score',
-    'round'
+    'round',
+    'minutes'
 ]
 player_column_suffixes = [
     'id',
@@ -63,5 +78,15 @@ winners_serve_columns = ['w_' + suffix for suffix in serve_column_suffixes]
 losers_profile_columns = ['loser_' + suffix for suffix in player_column_suffixes]
 losers_serve_columns = ['l_' + suffix for suffix in serve_column_suffixes]
 
-winners_data = complete_dataset[basic_columns + winners_profile_columns + winners_serve_columns]
-losers_data = complete_dataset[basic_columns + losers_profile_columns + losers_serve_columns]
+winners_data = filtered_tournaments[basic_columns + winners_profile_columns + winners_serve_columns].copy()
+losers_data = filtered_tournaments[basic_columns + losers_profile_columns + losers_serve_columns].copy()
+
+# %%
+# Removing the prefix of the columns
+remove_prefix(winners_data, 'winner_')
+remove_prefix(winners_data, 'w_')
+remove_prefix(losers_data, 'loser_')
+remove_prefix(losers_data, 'l_')
+
+# %%
+losers_data
